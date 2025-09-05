e34acc1e493bbaa476339606a09d30272787d136
ghost16-cyber <pallavenm007@gmail.com>
2025-09-06 00:41:34 +0400

    MGS

--- app/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.get("/", (req, res) => {
-  res.send("🚀 Hello from Dockerized Node.js App!");
-});
-
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
